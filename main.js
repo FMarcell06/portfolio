c9883c3ab8dc461322f7b2bfcb7450cd42cac983
FMarcell06 <forgoo.marcell0@gmail.com>
2025-03-20 10:59:48 +0100

    hero section updated, main script written

--- main.js
+++ main.js
@@ -1,2 +1,40 @@
 const url = `https://raw.githubusercontent.com/FMarcell06/portfolio_json/refs/heads/main/portfolio`
 
+getData(url,renderPortfolio)
+
+function renderPortfolio(data){
+    console.log(data);
+    users = data
+    data.forEach(obj=>{
+        console.log(obj);
+        document.querySelector('.workHolder').innerHTML+=`
+                <div class="workContent">
+                    <div class="portfolio">
+                        <div class="portfolioImg">
+                            <img src="${obj.photo_url}" class="portfolioKep" alt="">
+                        </div>
+                        <div class="portfolioContent">
+                            <div class="portfolioTitle">
+                                <h1>${obj.title}</h1>
+                            </div>
+                            <div class="portfolioTopics">
+                                <p>Témakörök:</p>
+                                <ul>
+                                    <li>asdasd</li>
+                                    <li>asdasd</li>
+                                    <li>asdasd</li>
+                                    <li>asdasd</li>
+                                    <li>asdasd</li>
+                                </ul>
+                            </div>
+                            <div class="portfolioLinks">
+                                <a target="_blank"href="${obj.repo_link}">Github link</a>
+                                <a target="_blank" href="${obj.site_link}">Weboldal link</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        
+        `
+    })
+}
